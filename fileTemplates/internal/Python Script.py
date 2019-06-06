d4990366b9e8e53a7358a4b20ab3d4b2fb55f823
Gilgamesh-Enkidu <675530973@qq.com>
2019-06-06 14:33:49 +0800

    PC-2019.1.2 <Gilgamesh@DESKTOP-CEQVAHP Create git.xml, project.default.xml, laf.xml, jdk.table.xml, Default.xml, code.style.schemes, find.xml, filetypes.xml, Python Script.py, editor.codeinsight.xml, plugin_ui.xml, colors.scheme.xml, External Tools.xml, editor.xml, github.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Author         : Gilgamish_Enkidu
+@Email          : Gilgamish_Enkidu@163.com
+
+@PROJECT_NAME   : ${PROJECT_NAME}
+@File           : ${NAME}.py
+@Software       : ${PRODUCT_NAME}
+@Time           : ${DATE} ${TIME}
+"""
+
