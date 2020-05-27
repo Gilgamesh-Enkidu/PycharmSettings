d37a81a37aab819747f9d9f18501b1868c349451
thanatos518 <675530973@qq.com>
2020-05-27 15:10:53 +0800

    PY-2020.1.1 <Thanatos@DESKTOP-5PKFS5M Create wsl.distributions.xml, filetypes.xml, Python Script.py, vcs.xml, colors.scheme.xml, External Tools.xml, laf.xml, editor.xml, jdk.table.xml, Default.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Author         : thanatos518
+@Email          : Gilgamish_Enkidu@163.com
+@PROJECT_NAME   : ${PROJECT_NAME}
+@File           : ${NAME}.py
+@Software       : ${PRODUCT_NAME}
+@Time           : ${DATE} ${TIME}
+"""
